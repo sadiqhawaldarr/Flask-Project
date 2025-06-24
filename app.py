3e0c95b78ec76ab20af5d45be56eb5118950f8e5
sadiqhawaldar <124356899+sadiqhawaldar@users.noreply.github.com>
2025-06-25 00:28:38 +0530

    Added the app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return '<html><body><h1>Hello World</h1></body></html>'
+   return '<html><body><h1>WELCOME</h1></body></html>'
 
 if __name__ == '__main__':
    app.run(debug = True)
